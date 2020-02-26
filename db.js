c88b5d5b7f67879766790b2e24216de98510aa31
jkarelins <jkarelins@gmail.com>
2020-02-26 17:50:12 +0100

    db returned back

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const databaseUrl =
   "postgres://postgres:password@localhost:5432/postgres";
 const db = new Sequelize(databaseUrl);
 
-db.sync({ force: true }) //.sync({ force: true }) confirms that it's OK to force changes in previously created tables, even though it results in data loss.
+db.sync() //.sync({ force: true }) confirms that it's OK to force changes in previously created tables, even though it results in data loss.
   .then(() => console.log("\x1b[32m", "DB updated"))
   .catch(() => console.log("Error updating table"));
 
