119f6f22ff66106b325a643c6addafb4d5268f29
jkarelins <jkarelins@gmail.com>
2020-02-26 10:20:15 +0100

    express setup

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`App is running on port ${port}!`));
