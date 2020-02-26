8ea339688a0097e8f519018a2900ddfdb2179ea3
jkarelins <jkarelins@gmail.com>
2020-02-26 12:35:07 +0100

    route for /team/:id ready

--- team/router.js
+++ team/router.js
@@ -8,18 +8,29 @@ router.get("/team", (req, res, next) => {
   Team.findAll()
     .then(teams => res.json(teams))
     .catch(err => next(err));
-  // res.send("all teams");
 });
 
 router.post("/team", (req, res, next) => {
-  // console.log(req.body);
   Team.create(req.body)
     .then(newTeam => {
-      // console.log(newTeam);
       res.json(newTeam);
     })
     .catch(err => res.status(404).end());
-  res.status(200).end();
+});
+
+router.get("/team/:id", (req, res) => {
+  const id = req.params.id;
+  Team.findByPk(id)
+    .then(team => {
+      if (team) {
+        res.json(team);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      return next(err);
+    });
 });
 
 module.exports = router;
