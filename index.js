d11c18cc9680b5dc5323a37e8839cbaa23f7bb7f
jkarelins <jkarelins@gmail.com>
2020-02-26 12:23:12 +0100

    /team - POST route

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
-const teamRouter = require("./team/router");
-
 const port = process.env.PORT || 4000;
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const teamRouter = require("./team/router");
 const DB = require("./db");
 const { Team } = require("./team/model");
 
+app.use(jsonParser);
 app.use(teamRouter);
 
 app.listen(port, () => console.log(`App is running on port ${port}!`));
--- team/router.js
+++ team/router.js
@@ -2,13 +2,24 @@ const { Router } = require("express");
 const router = new Router();
 const { Team } = require("./model");
 
-router.get("/", (req, res) => res.send("Hello World!"));
+// router.get("/", (req, res) => res.redirect("/team"));
 
 router.get("/team", (req, res, next) => {
   Team.findAll()
     .then(teams => res.json(teams))
     .catch(err => next(err));
   // res.send("all teams");
+});
+
+router.post("/team", (req, res, next) => {
+  // console.log(req.body);
+  Team.create(req.body)
+    .then(newTeam => {
+      // console.log(newTeam);
+      res.json(newTeam);
+    })
+    .catch(err => res.status(404).end());
+  res.status(200).end();
 });
 
 module.exports = router;
